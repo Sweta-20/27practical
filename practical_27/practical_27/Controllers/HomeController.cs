849dffdb6c1d54e37a044f6e343190a5f979ca48
SF-CPU-311\deval <deval.p@simformsolutions.com>
2022-04-06 12:39:36 +0530

    use global directories features from C# 10(practical-27)

---
+++ practical_27/practical_27/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿namespace Practical_15.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+		private readonly ILogger _logger;
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+		[HttpGet]
+		public IActionResult Get()
+		{
+			_logger.LogInformation("Homepage was requested");
+			try
+			{
+				throw new Exception("Oops! An Exception is going on!");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex.ToString());
+			}
+			return Ok("Hello World APi");
+		}
+	}
+}
---
+++ practical_27/practical_27/Globaldirectory.cs
@@ -0,0 +1,12 @@
+﻿//c#10 feature -- Global using directive
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Logging;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
